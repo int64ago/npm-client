e801f93016d9626a88ce76dfc9c88531e3fcf8f8
Cody Chan <int64ago@gmail.com>
2019-01-02 20:12:31 +0800

    test: remove cnpm

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ const test = require('ava');
 const npmClient = require('./index');
 
 test('should get npm clients', t => {
-  t.deepEqual(npmClient(), ['cnpm', 'yarn', 'npm']);
+  t.deepEqual(npmClient(), ['yarn', 'npm']);
 });
